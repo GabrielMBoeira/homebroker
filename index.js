a4252e102d39619ba48bdebffc825b73f11ae5cc
Gabriel <gboeira777@gmail.com>
2022-01-04 00:58:11 -0300

    Ajuste na posição da carteira

--- index.js
+++ index.js
@@ -4,27 +4,33 @@ const symbol = process.env.SYMBOL;
 
 setInterval(async () => {
 
-    // let buy = 0, sell = 0;
-    // const result = await api.depth(symbol);
+    let buy = 0, sell = 0;
+    const result = await api.depth(symbol);
 
-    // if (result.bids && result.bids.length) {
-    //     console.log(`Ordem de compra - Quem quer comprar ${result.bids[0][0]}`);
-    //     buy = parseFloat(result.bids[0][0]);
-    // }
+    if (result.bids && result.bids.length) {
+        console.log(`Ordem de compra - Quem quer comprar ${result.bids[0][0]}`);
+        buy = parseFloat(result.bids[0][0]);
+    }
 
-    // if (result.asks && result.asks.length) {
-    //     console.log(`Ordem de venda - Quem quer vender ${result.asks[0][0]}`);
-    //     sell = parseFloat(result.asks[0][0]);
-        console.log(await api.accountInfo());
-    // }
+    if (result.asks && result.asks.length) {
+        console.log(`Ordem de venda - Quem quer vender ${result.asks[0][0]}`);
+        sell = parseFloat(result.asks[0][0]);
+    }
 
-    // if (sell < 300) {
-    //     console.log('Hora de comprar');
-    // } else if (buy > 500) {
-    //     console.log('Hora de vender');
-    // } else {
-    //     console.log('Aguardando mercado');
-    // }
+    if (sell < 450) {
+        console.log('Hora de comprar');
+
+        const account = await api.accountInfo();
+        
+        const coins = account.balances.filter(b => symbol.indexOf(b.asset) !== -1);
+        console.log('POSIÇÃO DA CARTEIRA');
+        console.log(coins);
+
+    } else if (buy > 500) {
+        console.log('Hora de vender');
+    } else {
+        console.log('Aguardando mercado');
+    }
 
 
 }, process.env.CRAWLER_INTERVAL)
